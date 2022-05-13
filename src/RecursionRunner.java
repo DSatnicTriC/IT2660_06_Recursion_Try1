60a69ec61ec9a2c4c75fdea3a8174ae04db3e99a
Daniel Satnic <S01031109@acad.tri-c.edu>
2022-05-13 16:43:30 -0400

    generated 100 random numbers and printed them out

--- src/RecursionRunner.java
+++ src/RecursionRunner.java
@@ -1,9 +1,25 @@
+import java.util.Arrays;
+import java.util.Random;
 
 public class RecursionRunner {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		var ourArray = getRandomNumbers(100, -10, 100);
 
+		System.out.println("The array before any sorting");
+		System.out.println(Arrays.toString(ourArray));
+	}
+	
+	private static int[] getRandomNumbers(int arraySize, int arrayMin, int arrayMax) {
+		var newArray = new int[arraySize];
+		
+		Random rand = new Random();
+		
+		for (int i = 0; i < newArray.length; i++) {
+			newArray[i] = rand.nextInt(arrayMin, arrayMax);
+		}
+		
+		return newArray;
 	}
 
 }
